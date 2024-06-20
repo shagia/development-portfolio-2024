5b8aaa9b78f080aaff7cf2697b1341887ffa55f1
Shagia <usenet@shagia.me>
2024-06-20 03:42:00 -0700

    Gives front page thumbnails link attributes

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Link } from "gatsby"
 import "./galleryLayout.css"
 
 const Gallery = ({ posts }) => {
@@ -8,13 +9,16 @@ const Gallery = ({ posts }) => {
 			<div className="lightbox-container">
 				{
 					posts.map((post, key) => {
+						console.log(post.frontmatter.slug)
 						console.log(post.frontmatter.featuredImage)
 						return (
 							<div key={key} className="post">
 								<div className="post-overlay-container">
+								<Link to={`/${post.frontmatter.slug}`}>
 									<div className="post-image-container">
 										<img alt={post.frontmatter.imageAlt[0]} src={post.frontmatter.featuredImage.publicURL} />
 									</div>
+								</Link>
 								</div>
 							</div>
 						)
